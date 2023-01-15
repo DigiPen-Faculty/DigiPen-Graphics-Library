2df39b8d463df9075ef1f569394c3b00d8fea6b9
Andy Ellinger <andrea.ellinger@digipen.edu>
2023-01-15 14:05:17 -0800

    Updated header to new revision

--- DigiPen_Graphics_Library/src/DGL.h
+++ DigiPen_Graphics_Library/src/DGL.h
@@ -2,7 +2,7 @@
 // file:    DGL.h
 // author:  Andy Ellinger
 // brief:   DGL API interface 
-//          v1.0.1
+//          v1.0.2
 //          Documentation: https://github.com/DigiPen-Faculty/DigiPen-Graphics-Library/wiki 
 //
 // Copyright © 2022 DigiPen, All rights reserved.
