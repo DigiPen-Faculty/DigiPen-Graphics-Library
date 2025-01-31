01d1a6962116045191f3e18fbc8b57c93dfd1ca5
Andy Ellinger <andrea.ellinger@digipen.edu>
2025-01-31 13:49:53 -0800

    Updated to version 1.4.0

--- DGL/inc/DGL.h
+++ DGL/inc/DGL.h
@@ -2,7 +2,7 @@
 // file:    DGL.h
 // author:  Andy Ellinger
 // brief:   DGL API interface 
-//          v1.3.2
+//          v1.4.0
 //          Documentation: https://github.com/DigiPen-Faculty/DigiPen-Graphics-Library/wiki 
 //
 // Copyright © 2024 DigiPen, All rights reserved.
@@ -305,6 +305,14 @@ DGL_API DGL_Texture* DGL_Graphics_LoadTexture(const char* fileName);
 // Returns a pointer to the new texture instance.
 DGL_API DGL_Texture* DGL_Graphics_LoadTextureFromMemory(const unsigned char* data, int width, int height);
 
+// Creates a texture that will be used for drawing to.
+// It can also be drawn to the screen like a normal texture, once it has data.
+DGL_API DGL_Texture* DGL_Graphics_CreateRenderTexture(int width, int height);
+
+// Clears the specified render texture and fills it with the specified color.
+// Note: if the alpha of this color is not zero, drawing transparent textures onto it may not work well.
+DGL_API void DGL_Graphics_ClearRenderTexture(const DGL_Texture* renderTexture, const DGL_Color* color);
+
 // Unloads the provided texture from memory.
 // The pointer passed in will be set to NULL.
 DGL_API void DGL_Graphics_FreeTexture(DGL_Texture** texture);
@@ -358,16 +366,16 @@ DGL_API void DGL_Graphics_FinishDrawing(void);
 // Draws the provided mesh with the provided mode.
 DGL_API void DGL_Graphics_DrawMesh(const DGL_Mesh* mesh, DGL_DrawMode mode);
 
+// Draws the provided mesh to the provided render texture, with the provided mode.
+DGL_API void DGL_Graphics_DrawMeshToTexture(const DGL_Mesh* mesh, DGL_DrawMode mode, 
+    const DGL_Texture* renderTexture);
+
 //-------------------------------------------------------------------------------------------------
 // *** Constant buffer ****************************************************************************
 
 // Sets the position, scale, and rotation (in radians) which will be used for drawing meshes.
 DGL_API void DGL_Graphics_SetCB_TransformData(const DGL_Vec2* position, const DGL_Vec2* scale, 
     float rotationRadians);
-
-// Sets the Z layer value which will be used when drawing meshes.
-// Smaller values will make objects appear in front of other objects with larger values.
-DGL_API void DGL_Graphics_SetCB_ZLayer(float zValue);
 
 // Sets the transformation matrix with position, scale, and rotation to use when drawing meshes.
 DGL_API void DGL_Graphics_SetCB_TransformMatrix(const DGL_Mat4* transformationMatrix);
